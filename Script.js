328b3ff8ced6dfccf006091aa8431ae90c6d69e5
Kirtika Puniani <75201661+KirtikaPuniani@users.noreply.github.com>
2023-07-21 12:39:48 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,41 @@
+const questions = [
+    {
+        question: "Which is largest aminal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Elephant", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Giraffe", correct: false},
+        ]
+    },
 
+    {
+        question: "Which is smallest country in the world?",
+        answers: [
+            {text: "Vatican City", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "Sri Lanka", correct: false},
+        ]
+    },
+
+    {
+    question: "Which is largest desert in the world?",
+        answers: [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Antarctica", correct: false},
+            {text: "Sahara", correct: true},
+        ]
+    },
+
+    {
+        question: "Which is smallest continent in the world?",
+        answers: [
+            {text: "Africa", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Asia", correct: false},
+            {text: "Arctic", correct: false},
+        ]
+    }
+]
